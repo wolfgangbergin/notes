8cbd17e185e759b52cc91918f7ec56db28dfc973
wolfgangbergin <66983551+wolfgangbergin@users.noreply.github.com>
2022-07-16 16:33:13 -0400

    1st

--- vsCode/keyboardShortcuts.js
+++ vsCode/keyboardShortcuts.js
@@ -3,6 +3,7 @@ opt + cmd + f; // find replace
 cmd + d; // select next occurrence
 shift + opt + f; //  Prettier!!!
 opt + shift + downArrow; // Repeat line
+git branch -d <branch> // Delete branch
 
 let kimbo = 'kimbo4321';
 let kimbo = 'kimbo4321';
