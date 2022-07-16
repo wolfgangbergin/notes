976f75cc4dc9870788e1e2fa4dfe879cb10d2b66
wolfgangbergin <66983551+wolfgangbergin@users.noreply.github.com>
2022-07-16 16:27:58 -0400

    1st

--- vsCode/keyboardShortcuts.js
+++ vsCode/keyboardShortcuts.js
@@ -1,3 +1,13 @@
- F1 + printCode  // print 
-opt + cmd + f  // find replace
-cmd + d // select next occurrence
+F1 + printCode; // print
+opt + cmd + f; // find replace
+cmd + d; // select next occurrence
+shift + opt + f; //  Prettier!!!
+opt + shift + downArrow; // Repeat line
+
+let kimbo = 'kimbo4321';
+let kimbo = 'kimbo4321';
+let kimbo = 'kimbo4321';
+let kimbo = 'kimbo4321';
+let kimbo = 'kimbo4321';
+let kimbo = 'kimbo4321';
+
