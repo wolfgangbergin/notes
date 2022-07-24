708f40ade4cca76e75aadf57eeca14e0bceaf053
wolfgangbergin <66983551+wolfgangbergin@users.noreply.github.com>
2022-07-24 07:37:56 -0400

    2nt

--- gitHub/remoteCommands.js
+++ gitHub/remoteCommands.js
@@ -35,6 +35,26 @@
 // git checkout -b business
 
 
-
 // or ShortHand where dummy is the new Local branch branch name
 // or  git checkout -b dummy  origin/business
+
+
+
+// If you wish to set tracking information for this branch you can do so with:
+
+
+
+// origin = remote
+//git pull <remote> <branch>
+//  git remote add integer https://github.com/wolfgangbergin/though.git
+//git pull integer delete
+
+
+______________!!!
+//     git branch --set-upstream-to=<remote>/<branch> company
+
+// >> git branch -u integer/delete company
+// Branch 'company' set up to track remote branch 'delete' from 'integer'.
+______________!!!
+
+//  git push integer HEAD:delete
